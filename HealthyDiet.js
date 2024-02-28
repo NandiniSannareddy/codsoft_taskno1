094cd568627cea19d635fde90f4b944b450c15fd
NandiniSannareddy <150068076+NandiniSannareddy@users.noreply.github.com>
2024-02-28 20:32:14 +0530

    Add files via upload

---
+++ HealthyDiet.js
@@ -0,0 +1,22 @@
+const item1 = document.getElementById("item1");
+const item2 = document.getElementById("item2");
+const item3 = document.getElementById("item3");
+const item4 = document.getElementById("item4");
+const item = document.getElementById("item");
+
+function change1() {
+	item.src="breakfast.jpg";
+}
+
+function change2() {
+	item.src="lunch.jpg";
+}
+
+function change3() {
+	item.src="snacks.jpg";
+}
+
+function change4() {
+	item.src="dinner.jpg";
+}
+
